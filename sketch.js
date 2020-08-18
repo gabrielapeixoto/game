4017bf5d3d41b82e955b114f0e9a8726355a2d63
gabrielapeixoto <peixoto.sgabriela@gmail.com>
2020-08-18 17:47:30 -0300

    jogo imersão gamedev alura

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  somDoJogo.loop();
+  frameRate(45);
+  jogo = new Jogo();
+  telaInicial = new TelaInicial();
+  jogo.setup();
+  cenas = {
+    jogo,
+    telaInicial
+  };
+  botaoGerenciador = new BotaoGerenciador('Iniciar', width/2, height/2);
+}
+
+function keyPressed(){
+  jogo.keyPressed();
+}
+
+function draw() {
+  cenas[cenaAtual].draw();
+}
